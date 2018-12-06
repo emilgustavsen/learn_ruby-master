8b5be402282bafd0a717ff802c1c925c9da3f7b1
Emil Gustavsen <emilgustavsen@gmail.com>
2018-12-06 13:10:44 +0100

    started exercise 04 - pig latin now works with single words

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
-#write your code here
+# vowels = ['a','e','o','u','i']
+
+# def translate(string)
+# 	string = string.downcase
+# 	array = string.split(' ')
+# 	array = array.each |x| 
+		
+
+
+# end
+
+
+def translate(string)
+	string = string.downcase
+	# Multiple words
+	if string.include? ' '
+		i = string.index ' '
+		string = string.gsub(/^[aeuio].+/, string[0..i-1] + 'ay' + string[i..-1])
+		string = string.gsub(/^[^aeuio]{2}\S+/, string[2..string.length] + string[0..1] + 'ay')
+		string = string.gsub(/^[^aeuio]\S+/, string[1..string.length] + string[0] + 'ay')
+
+		string = string.gsub(/ [aeuio]\S+ /, string + 'ay')
+		string = string.gsub(/ [^aeuio]\S+ /, string + string[0] +'ay')
+	# Single words
+	else	
+		# Vowel single word
+		string = string.gsub(/^[aeuio].+/, string + 'ay')
+		# Two consonants
+		string = string.gsub(/^[^aeuio]{2}\S+/, string[2..string.length] + string[0..1] + 'ay')
+		# Single consonant (Needs to be after two)
+		string = string.gsub(/^[^aeuio]\S+/, string[1..string.length] + string[0] + 'ay')
+	end
+	
+end
