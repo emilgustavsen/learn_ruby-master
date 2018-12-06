5d070f25ccabfb58f4d74a5f177b1cc18537d073
Emil Gustavsen <emilgustavsen@gmail.com>
2018-12-06 09:26:02 +0100

    finished exercise 02 (third)

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
-#write your code here
+def add(num, num2)
+	num + num2
+end
+
+def subtract(num, num2)
+	num - num2
+end
+
+def sum(array)
+	array.reduce(0, :+)
+end
+
+def multiply(array)
+	# array = Array.[](num, num2)
+	array.reduce(1, :*)
+end
+
+def power(num, num2)
+	num**num2
+end
+
+def factorial(num)
+	(1..num).reduce(:*) || 1
+end
